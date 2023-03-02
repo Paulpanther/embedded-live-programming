fa49fd379fbbe73abe218ccba68dac7f3aeb5919
GitHub Action <action@github.com>
2023-03-02 18:12:38 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "embedded-live-programming"
--- src/main/kotlin/com/github/paulpanther/embeddedliveprogramming/services/MyProjectService.kt
+++ src/main/kotlin/com/github/paulpanther/embeddedliveprogramming/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.paulpanther.embeddedliveprogramming.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.paulpanther.embeddedliveprogramming.MyBundle
 
 class MyProjectService(project: Project) {
 
